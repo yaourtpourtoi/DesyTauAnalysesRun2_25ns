9ac4637ee575e05ea2610e7f531965687f1e14a9
Oleg Filatov <oleg.filatov@cern.ch>
2019-09-13 17:56:13 +0200

    crash somewhere in z pt weight, trying to spot

--- NTupleMaker/bin/SynchNTupleProducer_Run2.cpp
+++ NTupleMaker/bin/SynchNTupleProducer_Run2.cpp
@@ -963,32 +963,39 @@ int main(int argc, char * argv[]){
       TLorentzVector genV( 0., 0., 0., 0.);
       TLorentzVector genL( 0., 0., 0., 0.);
       
-      // // Zpt weight
-      // otree->zptweight = 1.;
-      // if (!isData && ((isDY && isMG ) || isEWKZ)){
-      //   genV = genTools::genV(analysisTree); // gen Z boson ?
-      // 	float bosonMass = genV.M();
-      // 	float bosonPt = genV.Pt();
-      // 
-      //   //Merijn determine here some min and max values:
-      //   double massxmin = h_zptweight->GetXaxis()->GetXmin();
-      //   double massxmax = h_zptweight->GetXaxis()->GetXmax();
-      // 
-      //   double ptxmin = h_zptweight->GetYaxis()->GetXmin();
-      //   double ptxmax = h_zptweight->GetYaxis()->GetXmax();
-      // 
-      // 	//Merijn 2019 6 13: adjust to T/M functions, to get boundaries right. Otherwise, for 2017 data we get few outliers that screw up the weight histogram dramatically.
-      // 	Float_t zptmassweight = 1;
-      // 	if (bosonMass > 50.0) {
-      //     float bosonMassX = bosonMass;
-      //     float bosonPtX = bosonPt;
-      //     if (bosonMassX > massxmax) bosonMassX = massxmax - h_zptweight->GetXaxis()->GetBinWidth(h_zptweight->GetYaxis()->GetNbins())*0.5;//Merijn: if doesn't work, lower by 1/2 binwidth..
-      //     if (bosonPtX < ptxmin)     bosonPtX = ptxmin + h_zptweight->GetYaxis()->GetBinWidth(1)*0.5;
-      //     if (bosonPtX > ptxmax)     bosonPtX = ptxmax - h_zptweight->GetYaxis()->GetBinWidth(h_zptweight->GetYaxis()->GetNbins())*0.5;
-      //     zptmassweight = h_zptweight->GetBinContent(h_zptweight->GetXaxis()->FindBin(bosonMassX), h_zptweight->GetYaxis()->FindBin(bosonPtX));
-      //     }	
-      //     otree->zptweight = zptmassweight;
-      // }
+      // Zpt weight
+      otree->zptweight = 1.;
+      if (!isData && ((isDY && isMG ) || isEWKZ)){
+        genV = genTools::genV(analysisTree); // gen Z boson ?
+      	float bosonMass = genV.M();
+      	float bosonPt = genV.Pt();
+        
+        std::cout << "HERE I AM [1]" << std::endl;
+  
+        //Merijn determine here some min and max values:
+        double massxmin = h_zptweight->GetXaxis()->GetXmin();
+        double massxmax = h_zptweight->GetXaxis()->GetXmax();
+      
+        double ptxmin = h_zptweight->GetYaxis()->GetXmin();
+        double ptxmax = h_zptweight->GetYaxis()->GetXmax();
+      
+        std::cout << "HERE I AM [2]" << std::endl;
+        
+      	//Merijn 2019 6 13: adjust to T/M functions, to get boundaries right. Otherwise, for 2017 data we get few outliers that screw up the weight histogram dramatically.
+      	Float_t zptmassweight = 1;
+      	if (bosonMass > 50.0) {
+          float bosonMassX = bosonMass;
+          float bosonPtX = bosonPt;
+          if (bosonMassX > massxmax) bosonMassX = massxmax - h_zptweight->GetXaxis()->GetBinWidth(h_zptweight->GetYaxis()->GetNbins())*0.5;//Merijn: if doesn't work, lower by 1/2 binwidth..
+          if (bosonPtX < ptxmin)     bosonPtX = ptxmin + h_zptweight->GetYaxis()->GetBinWidth(1)*0.5;
+          if (bosonPtX > ptxmax)     bosonPtX = ptxmax - h_zptweight->GetYaxis()->GetBinWidth(h_zptweight->GetYaxis()->GetNbins())*0.5;
+          
+          std::cout << "HERE I AM [3]" << std::endl;
+          
+          zptmassweight = h_zptweight->GetBinContent(h_zptweight->GetXaxis()->FindBin(bosonMassX), h_zptweight->GetYaxis()->FindBin(bosonPtX));
+          }	
+          otree->zptweight = zptmassweight;
+      }
       
       // topPt weight
       otree->topptweight = 1.;
