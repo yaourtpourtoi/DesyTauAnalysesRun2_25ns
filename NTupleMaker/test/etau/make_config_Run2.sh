f8fbf25d515b37c6b2e7a16c66b919f1a93ce21b
Oleg Filatov <oleg.filatov@cern.ch>
2020-04-09 16:21:19 +0200

    redesigned make_config

--- NTupleMaker/test/etau/make_config_Run2.sh
+++ NTupleMaker/test/etau/make_config_Run2.sh
@@ -1,27 +1,58 @@
 #!/bin/bash
 
-### the script is to be run with "./make_config_Run2.sh <year={16,17,18}> <data_type={data, MC, embedded}>"
+### the script is to be run with "./make_config_Run2.sh <year={16,17,18}> <data_type={data, MC, embedded}> <channel={mt,et}>"
 
 YEAR=$1
 DATA_TYPE=$2
+CHANNEL=$3
 OUTDIR=./20$YEAR
+TEMPLATE_CFG_PREFIX="analysisMacroSynch"
+TEMPLATE_CFG_NAME=${TEMPLATE_CFG_PREFIX}_${CHANNEL}_${YEAR}_${DATA_TYPE}
 
 if [ ! -d "$OUTDIR" ]; then
   mkdir $OUTDIR
 fi
 
-if [[ $YEAR -ne 16 && $YEAR -ne 17 && $YEAR -ne 18 ]]; then
+if [[ $CHANNEL != "mt" || $CHANNEL == "et" ]]; then
   echo
   echo "To produce the scripts for a specific year and either data or MC this script is to be run with a command:"
   echo
-  echo "  bash make_config_Run2.sh <year={16,17,18}> <data_type={data, MC, embedded}>"
+  echo "  ./make_config_Run2.sh <year={16,17,18}> <data_type={data, MC, embedded}> <channel={mt,et}>"
   echo
-  echo "year is not 16, 17 or 18 - exiting"
+  echo "channel is not mt or et - exiting"
   exit
 fi
 
-# pick the template for the corresponding year
-TEMPLATE_CFG_NAME="analysisMacroSynch_et_${YEAR}"
+if [[ $YEAR -eq 16 ]]; then
+  NOT_YEAR=(17 18)
+else 
+  if [[ $YEAR -eq 17 ]]; then
+    NOT_YEAR=(16 18)
+  else 
+    if [[ $YEAR -eq 18 ]]; then
+      NOT_YEAR=(16 17)
+    else 
+      echo
+      echo "To produce the scripts for a specific year and either data or MC this script is to be run with a command:"
+      echo
+      echo "  ./make_config_Run2.sh <year={16,17,18}> <data_type={data, MC, embedded}> <channel={mt,et}>"
+      echo
+      echo "year is not 16, 17 or 18 - exiting"
+      exit
+    fi
+  fi
+fi
+
+cat ${TEMPLATE_CFG_PREFIX}_common.conf ${TEMPLATE_CFG_PREFIX}_${CHANNEL}.conf > ${TEMPLATE_CFG_NAME}_tmp.conf
+
+# remove all the lines for years which is not the one specified 
+NOT_YEAR_LEN=${#NOT_YEAR[@]}
+for (( i = 0; i < NOT_YEAR_LEN; i++ )); do
+  sed -i "/${NOT_YEAR[i]}: /d" ${TEMPLATE_CFG_NAME}_tmp.conf
+done
+
+# remove just the strings "YEAR: " leaving the rest of the line intact 
+sed -i "s/${YEAR}: //" ${TEMPLATE_CFG_NAME}_tmp.conf
 
 # define parameters which are different between MC and data configs
 KEY_LIST=(isData ApplyPUweight ApplyLepSF ApplyRecoilCorrections ApplyBTagScaling)
@@ -65,7 +96,7 @@ else
       echo
       echo "To produce the scripts for a specific year and either data or MC this script is to be run with a command:"
       echo
-      echo "  bash make_config_Run2.sh <year={16,17,18}> <data_type={data, MC, embedded}>"
+      echo "  ./make_config_Run2.sh <year={16,17,18}> <data_type={data, MC, embedded}> <channel={mt,et}>"
       echo
       echo "data_type is neither data nor MC - exiting"
       exit
@@ -77,16 +108,19 @@ fi
 KEY_LEN=${#KEY_LIST[@]}
 for (( i = 0; i < $KEY_LEN; i++ )); do
         printf '/%s/c\%s\n' "${KEY_LIST[i]} =*" "${KEY_LIST[i]} = ${VALUE_LIST[i]}"
-done | sed -r -f- $TEMPLATE_CFG_NAME.conf > ${TEMPLATE_CFG_NAME}_${DATA_TYPE}.conf
+done | sed -r -i -f- ${TEMPLATE_CFG_NAME}_tmp.conf
+
+# add year
+sed -i "s/era = /era = 20$YEAR/" ${TEMPLATE_CFG_NAME}_tmp.conf
 
 # remove all the lines which starts with "NOT_DATA_TYPE: " 
 NOT_DATA_TYPE_LEN=${#NOT_DATA_TYPE[@]}
 for (( i = 0; i < NOT_DATA_TYPE_LEN; i++ )); do
-  sed -i "/${NOT_DATA_TYPE[i]}: /d" ${TEMPLATE_CFG_NAME}_${DATA_TYPE}.conf
+  sed -i "/${NOT_DATA_TYPE[i]}: /d" ${TEMPLATE_CFG_NAME}_tmp.conf
 done
 
 # remove just the strings "DATA_TYPE: " leaving the rest of the line intact 
-sed -i "s/${DATA_TYPE}: //" ${TEMPLATE_CFG_NAME}_${DATA_TYPE}.conf
+sed -i "s/${DATA_TYPE}: //" ${TEMPLATE_CFG_NAME}_tmp.conf
 
 # lists with the MC samples' names
 MC_SAMPLES_LIST=(DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8 DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8 DY2JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8)
@@ -100,13 +134,13 @@ if [[ $DATA_TYPE == "MC" ]]; then
   if [[ $YEAR -eq 17 ]]; then # for 17 the path in the root file to PU histograms is sample-dependent, pick it from the list
     for (( i = 0; i < $MC_SAMPLES_LEN; i++ )); do
         PU_STR=${MC_SAMPLES_LIST[i]}_pileup
-        sed "s/pileUpforMC =/pileUpforMC = ${PU_STR}/" ${TEMPLATE_CFG_NAME}_${DATA_TYPE}.conf > $OUTDIR/analysisMacroSynch_et_${MC_SAMPLES_LIST[i]}.conf
+        sed "s/pileUpforMC =/pileUpforMC = ${PU_STR}/" ${TEMPLATE_CFG_NAME}_tmp.conf > $OUTDIR/${TEMPLATE_CFG_NAME}_${MC_SAMPLES_LIST[i]}.conf
     done
-    sed "s/pileUpforMC =/pileUpforMC = MC_PU2017_pileup/" ${TEMPLATE_CFG_NAME}_${DATA_TYPE}.conf > $OUTDIR/${TEMPLATE_CFG_NAME}_${DATA_TYPE}.conf
+    sed "s/pileUpforMC =/pileUpforMC = MC_PU2017_pileup/" ${TEMPLATE_CFG_NAME}_tmp.conf > $OUTDIR/${TEMPLATE_CFG_NAME}.conf
   else
-    sed "s/pileUpforMC =/pileUpforMC = pileup/" ${TEMPLATE_CFG_NAME}_${DATA_TYPE}.conf > $OUTDIR/${TEMPLATE_CFG_NAME}_${DATA_TYPE}.conf
+    sed "s/pileUpforMC =/pileUpforMC = pileup/" ${TEMPLATE_CFG_NAME}_tmp.conf > $OUTDIR/${TEMPLATE_CFG_NAME}.conf
   fi # path in the root file to PU histograms for 16 and 18 data; 
 else
-  cp  ${TEMPLATE_CFG_NAME}_${DATA_TYPE}.conf $OUTDIR/${TEMPLATE_CFG_NAME}_${DATA_TYPE}.conf
+  cp  ${TEMPLATE_CFG_NAME}_tmp.conf $OUTDIR/${TEMPLATE_CFG_NAME}.conf
 fi
-rm ${TEMPLATE_CFG_NAME}_${DATA_TYPE}.conf
+rm ${TEMPLATE_CFG_NAME}_tmp.conf
