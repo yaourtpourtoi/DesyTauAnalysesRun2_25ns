d391f4c12e1bc615a2b5becc022c480c8081a015
Oleg Filatov <oleg.filatov@cern.ch>
2020-04-14 18:30:51 +0200

    TES for embedded, data zeroed in cfg

--- NTupleMaker/test/etau/make_config_Run2.sh
+++ NTupleMaker/test/etau/make_config_Run2.sh
@@ -64,31 +64,16 @@ VALUE_LIST_EMBEDDED=(true false true false false)
 # also redefine list of the parameters according to the input data type
 
 if [[ $DATA_TYPE == "data" ]]; then
-  KEY_LIST+=(TauEnergyScaleShift_OneProng TauEnergyScaleShift_OneProngOnePi0 TauEnergyScaleShift_ThreeProng)
-  VALUE_LIST_DATA+=(0.0 0.0 0.0)
-  
-  KEY_LIST+=(TauEnergyScaleShift_LepFake_OneProng TauEnergyScaleShift_LepFake_OneProngOnePi0 TauEnergyScaleShift_LepFake_ThreeProng)
-  VALUE_LIST_DATA+=(0.0 0.0 0.0)
-
-  KEY_LIST+=(TauEnergyScaleShift_OneProng_Error TauEnergyScaleShift_OneProngOnePi0_Error TauEnergyScaleShift_ThreeProng_Error)
-  VALUE_LIST_DATA+=(0.0 0.0 0.0)  
-
   VALUE_LIST=("${VALUE_LIST_DATA[@]}")
   NOT_DATA_TYPE=("MC" "embedded")
 else
   if [[ $DATA_TYPE == "MC" ]]; then
     VALUE_LIST=("${VALUE_LIST_MC[@]}")
     NOT_DATA_TYPE=("data" "embedded")
-  else 
-    if [[ $DATA_TYPE == "embedded" ]]; then 
-      KEY_LIST+=(TauEnergyScaleShift_OneProng TauEnergyScaleShift_OneProngOnePi0 TauEnergyScaleShift_ThreeProng)
-      VALUE_LIST_EMBEDDED+=(0.0 0.0 0.0)
-
-      KEY_LIST+=(TauEnergyScaleShift_LepFake_OneProng TauEnergyScaleShift_LepFake_OneProngOnePi0 TauEnergyScaleShift_LepFake_ThreeProng)
-      VALUE_LIST_EMBEDDED+=(0.0 0.0 0.0)
-     
-      KEY_LIST+=(TauEnergyScaleShift_OneProng_Error TauEnergyScaleShift_OneProngOnePi0_Error TauEnergyScaleShift_ThreeProng_Error)
-      VALUE_LIST_EMBEDDED+=(0.015 0.015 0.015) 
+  else    
+    if [[ $DATA_TYPE == "embedded" ]]; then    
+      KEY_LIST+=(TauEnergyScaleShift_LepFake_OneProng TauEnergyScaleShift_LepFake_OneProngOnePi0 TauEnergyScaleShift_LepFake_ThreeProng TauEnergyScaleShift_LepFake_ThreeProngOnePi0)
+      VALUE_LIST_EMBEDDED+=(0.0 0.0 0.0 0.0)
       
       VALUE_LIST=("${VALUE_LIST_EMBEDDED[@]}")
       NOT_DATA_TYPE=("MC" "data")
